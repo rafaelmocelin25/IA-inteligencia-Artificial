4c21bafaf44ed38be1b142db4c7b3422934c9f85
rafaelmocelin25 <141454436+rafaelmocelin25@users.noreply.github.com>
2025-05-23 11:19:52 -0300

    SADASD

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-alternativas");
+const textoResultado = document.querySelector(".texto-resultado");
+Const perguntas = [
+    {
+        enunciado: "pergunta1",
+        alternativas: ["aternativa1","alternativa2"]
+    },
+    {
+            enunciado: "pergunta2",
+        alternativas: ["aternativa1","alternativa2"]
+    },
+];
