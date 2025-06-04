79ceaa7f50661524f0efd048145b5bae194f2166
rafaelmocelin25 <141454436+rafaelmocelin25@users.noreply.github.com>
2025-06-04 10:06:40 -0300

    fdsfdsf

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-alternativas");
 const textoResultado = document.querySelector(".texto-resultado");
-Const perguntas = [
+const perguntas = [
     {
         enunciado: "Como podemos aproveitar eventos culturais como a Semana Farroupilha, com tradições como a costela de chão para promover o respeito à diversidade e a conscientização sobre problemas atuais, como os maus-tratos aos animais e a extinção de espécies?",
         alternativas:[
